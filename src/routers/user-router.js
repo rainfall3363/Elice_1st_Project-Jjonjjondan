98c02914969a87afcf9c3f2153b7b4dac8573720
[SW2/서울/2반] 추효진 <rainfall3363@gmail.com>
2022-05-24 17:41:26 +0900

    getUserInfo API 구현

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -67,9 +67,19 @@ userRouter.get('/userlist', loginRequired, async function (req, res, next) {
   try {
     // 전체 사용자 목록을 얻음
     const users = await userService.getUsers();
-
     // 사용자 목록(배열)을 JSON 형태로 프론트에 보냄
     res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 특정 사용자 정보 불러오기
+userRouter.get('/userInfo', loginRequired, async function (req, res, next) {
+  try {
+    const userInfo = await userService.getUserInfo(req.currentUserId);
+
+    res.status(200).json(userInfo);
   } catch (error) {
     next(error);
   }
