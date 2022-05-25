1b8f7dd31647e904511ad37e979643dc1605c6be
Circlewee <pnj1118@naver.com>
2022-05-25 21:47:48 +0900

    회원 탈퇴 api연동 및 구현

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -11,6 +11,7 @@ viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/cart', serveStatic('cart'));
 viewsRouter.use('/detail', serveStatic('detail'));
+viewsRouter.use('/account', serveStatic('account'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
--- src/views/account/account.js
+++ src/views/account/account.js
@@ -1,14 +1,18 @@
 import * as Api from '../api.js';
 import { loginUser, logoutUser } from '../useful-functions.js';
 
+const deleteButton = document.querySelector('#deleteButton');
+
 (() => {
   renderUserInfo();
   loginUser();
   logoutUser();
-  // addAllEvents();
+  addAllEvents();
 })();
 
-function addAllEvents() {}
+function addAllEvents() {
+  deleteButton.addEventListener('click', deleteUserInfo);
+}
 
 async function renderUserInfo() {
   const user = await Api.get('/api/userInfo');
@@ -27,4 +31,15 @@ async function addUserInfo(user) {
   userAddress.innerText = user.address
     ? `${user.address.address1} ${user.address.address2}`
     : '주소 정보가 없습니다.';
+}
+
+async function deleteUserInfo() {
+  try {
+    const result = await Api.delete('/api/userDelete');
+    alert(`${result.fullName}님의 계정이 탈퇴되었습니다.`);
+    sessionStorage.removeItem('token');
+    window.location.href = '/';
+  } catch (err) {
+    alert('회원 탈퇴처리가 정상적으로 진행되지 않았습니다. 다시 시도해주세요.');
+  }
 }
