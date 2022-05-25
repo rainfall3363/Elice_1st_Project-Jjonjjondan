2be564144175053bd602520b15815e8c197ca08a
[SW2/서울/2반] 김종한 <jhkcompany710@gmail.com>
2022-05-25 12:25:08 +0900

    last commit

---
+++ src/db/schemas/product-schema.js
@@ -0,0 +1,38 @@
+// 평가기준표 기반 작성
+import { Schema } from 'mongoose';
+// 상품번호, 상품명, 가격, 설명, 제조사, 카테고리
+const ProductSchema = new Schema(
+  {
+    productId: {
+      // 추후 nanoid로 구현
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    maker: {
+      type: String,
+      required: false,
+    },
+    category: {
+      type: Number, // 카테고리:1 -> 남자셔츠, 2 -> 여자셔츠 ...
+      required: false,
+    },
+  },
+  {
+    collection: 'products',
+    timestamps: true,
+  }
+);
+
+export { ProductSchema };
