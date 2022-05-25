d8aa330ab8410e7ff58aa13fe0c865390355af2e
cyberjam <jamsince1994@naver.com>
2022-05-25 16:55:21 +0900

    indexedDB에서 get한 데이터로 장바구니 리스트 화면 구성

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,7 +1,5 @@
 function getCartList() {
-  let productsCardsElement = document.getElementsByClassName(
-    'cart-products-cards'
-  )[0];
+  let productsCardsElement = document.getElementById('cartList');
 
   let store = db.transaction('order', 'readonly').objectStore('order');
   let getAllReq = store.getAll();
@@ -12,13 +10,59 @@ function getCartList() {
     let resultCards = '';
     for (let i = 0; i < productsCards.length; i++) {
       resultCards += `
-                    <div class="card">
-                        <div class="container">
-                            <h4><b>${productsCards[i].product}</b></h4>
-                            <p>${productsCards[i].price}</p>
-                        </div>
-                    </div>
-                `;
+        <div class="cart-product-item" id="productItem-{_id}">
+          <label class="checkbox">
+            <input type="checkbox" id="checkbox-{_id}" checked />
+          </label>
+          <button class="delete-button" id="delete-{_id}">
+            <span class="icon">
+              <i class="fas fa-trash-can"></i>
+            </span>
+          </button>
+          <figure class="image is-96x96">
+            <img id="image-{_id}" src="https://ifh.cc/g/o5yDfg.jpg" alt="product-image" />
+          </figure>
+          <div class="content">
+            <p id="title-{_id}">${productsCards[i].product}</p>
+            <div class="quantity">
+              <button class="button is-rounded" id="minus-{_id}" disabled>
+                <span class="icon is-small">
+                  <i class="fas fa-thin fa-minus"></i>
+                </span>
+              </button>
+              <input
+                class="input"
+                id="quantityInput-{_id}"
+                type="number"
+                min="1"
+                max="99"
+                value="1"
+              />
+              <button class="button is-rounded" id="plus-{_id}">
+                <span class="icon">
+                  <i class="fas fa-lg fa-plus"></i>
+                </span>
+              </button>
+            </div>
+          </div>
+          <div class="calulation">
+            <p id="unitPrice-{_id}">${productsCards[i].price}</p>
+            <p>
+              <span class="icon">
+                <i class="fas fa-thin fa-xmark"></i>
+              </span>
+            </p>
+            <p id="quantity-{_id}">제품 수량</p>
+            <p>
+              <span class="icon">
+                <i class="fas fa-thin fa-equals"></i>
+              </span>
+            </p>
+            <p id="total-{_id}">합산 값</p>
+          </div>
+        </div>
+
+      `;
     }
     productsCardsElement.innerHTML = resultCards;
   });
