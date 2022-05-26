ad13edb2a7cffdc25aa9e5946d16ff45c5ae4dc4
cyberjam <jamsince1994@naver.com>
2022-05-26 14:11:52 +0900

    체크박스 전체선택 해제 기능 완성

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,4 +1,64 @@
-function getCartList() {
+function makeProductsCard(productsCardsElement) {
+  return `
+    <div class="cart-product-item" id="productItem-${productsCardsElement.id}">
+      <label class="checkbox">
+        <input type="checkbox" class="cart-checkbox" id="checkbox-${productsCardsElement.id}"/>
+      </label>
+      <button class="delete-button" id="delete-${productsCardsElement.id}">
+        <span class="icon">
+          <i class="fas fa-trash-can"></i>
+        </span>
+      </button>
+      <figure class="image is-96x96">
+        <img
+          id="image-${productsCardsElement.id}"
+          src="https://ifh.cc/g/o5yDfg.jpg"
+          alt="product-image"
+        />
+      </figure>
+      <div class="content">
+        <p id="title-${productsCardsElement.id}">${productsCardsElement.product}</p>
+        <div class="quantity">
+          <button class="button is-rounded" id="minus-${productsCardsElement.id}" disabled>
+            <span class="icon is-small">
+              <i class="fas fa-thin fa-minus"></i>
+            </span>
+          </button>
+          <input
+            class="input"
+            id="quantityInput-${productsCardsElement.id}"
+            type="number"
+            min="1"
+            max="99"
+            value="1"
+          />
+          <button class="button is-rounded" id="plus-${productsCardsElement.id}">
+            <span class="icon">
+              <i class="fas fa-lg fa-plus"></i>
+            </span>
+          </button>
+        </div>
+      </div>
+      <div class="calulation">
+        <p id="unitPrice-${productsCardsElement.id}">${productsCardsElement.price}</p>
+        <p>
+          <span class="icon">
+            <i class="fas fa-thin fa-xmark"></i>
+          </span>
+        </p>
+        <p id="quantity-${productsCardsElement.id}">제품 수량</p>
+        <p>
+          <span class="icon">
+            <i class="fas fa-thin fa-equals"></i>
+          </span>
+        </p>
+        <p id="total-${productsCardsElement.id}">합산 값</p>
+      </div>
+    </div>
+  `;
+}
+
+function renderCartList() {
   let productsCardsElement = document.getElementById('cartList');
 
   let store = db.transaction('order', 'readonly').objectStore('order');
@@ -9,60 +69,7 @@ function getCartList() {
 
     let resultCards = '';
     for (let i = 0; i < productsCards.length; i++) {
-      resultCards += `
-        <div class="cart-product-item" id="productItem-{_id}">
-          <label class="checkbox">
-            <input type="checkbox" id="checkbox-{_id}" checked />
-          </label>
-          <button class="delete-button" id="delete-{_id}">
-            <span class="icon">
-              <i class="fas fa-trash-can"></i>
-            </span>
-          </button>
-          <figure class="image is-96x96">
-            <img id="image-{_id}" src="https://ifh.cc/g/o5yDfg.jpg" alt="product-image" />
-          </figure>
-          <div class="content">
-            <p id="title-{_id}">${productsCards[i].product}</p>
-            <div class="quantity">
-              <button class="button is-rounded" id="minus-{_id}" disabled>
-                <span class="icon is-small">
-                  <i class="fas fa-thin fa-minus"></i>
-                </span>
-              </button>
-              <input
-                class="input"
-                id="quantityInput-{_id}"
-                type="number"
-                min="1"
-                max="99"
-                value="1"
-              />
-              <button class="button is-rounded" id="plus-{_id}">
-                <span class="icon">
-                  <i class="fas fa-lg fa-plus"></i>
-                </span>
-              </button>
-            </div>
-          </div>
-          <div class="calulation">
-            <p id="unitPrice-{_id}">${productsCards[i].price}</p>
-            <p>
-              <span class="icon">
-                <i class="fas fa-thin fa-xmark"></i>
-              </span>
-            </p>
-            <p id="quantity-{_id}">제품 수량</p>
-            <p>
-              <span class="icon">
-                <i class="fas fa-thin fa-equals"></i>
-              </span>
-            </p>
-            <p id="total-{_id}">합산 값</p>
-          </div>
-        </div>
-
-      `;
+      resultCards += makeProductsCard(productsCards[i]);
     }
     productsCardsElement.innerHTML = resultCards;
   });
@@ -73,7 +80,7 @@ let db;
 
 dbReq.addEventListener('success', function (event) {
   db = event.target.result;
-  getCartList();
+  renderCartList();
 });
 
 dbReq.addEventListener('error', function (event) {
@@ -89,5 +96,20 @@ dbReq.addEventListener('upgradeneeded', function (event) {
       keyPath: 'id',
       autoIncrement: true,
     });
+  }
+});
+
+const checkboxElements = document.getElementsByClassName('cart-checkbox');
+const selectAllCheckboxElement = document.getElementById('allSelectCheckbox');
+
+selectAllCheckboxElement.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    for (let element of checkboxElements) {
+      element.checked = true;
+    }
+  } else {
+    for (let element of checkboxElements) {
+      element.checked = false;
+    }
   }
 });
