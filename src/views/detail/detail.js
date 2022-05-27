a71425c8e1b9a6386aa3a486dde8c49bc588b4da
cyberjam <jamsince1994@naver.com>
2022-05-27 23:13:19 +0900

    getLocalStorageList 내 cartList를 valueList로 변경

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,6 +1,6 @@
 function getLocalStorageList(key) {
-  let cartList = window.localStorage.getItem(key);
-  if (cartList === null) {
+  let valueList = window.localStorage.getItem(key);
+  if (valueList === null) {
     window.localStorage.setItem(key, JSON.stringify([]));
     return JSON.parse(window.localStorage.getItem(key));
   } else {
@@ -8,8 +8,14 @@ function getLocalStorageList(key) {
   }
 }
 
-let localStorageBtnElement = document.getElementById('localStorageReadBtn');
-localStorageBtnElement.addEventListener('click', function () {
+let localStorageGetBtnElement = document.getElementById('localStorageGetBtn');
+localStorageGetBtnElement.addEventListener('click', function () {
+  let cartList = getLocalStorageList('cart');
+  console.log(cartList);
+});
+
+let localStorageAddBtnElement = document.getElementById('localStorageAddBtn');
+localStorageAddBtnElement.addEventListener('click', function () {
   let cartList = getLocalStorageList('cart');
   console.log(cartList);
 });
