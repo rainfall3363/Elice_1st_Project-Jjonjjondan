bc385e7a2c6086d105ea06d35ab535d049076fee
cyberjam <jamsince1994@naver.com>
2022-05-27 23:35:19 +0900

    localStorage add 완성

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -8,6 +8,13 @@ function getLocalStorageList(key) {
   }
 }
 
+function addLocalStorageList(key, value) {
+  let valueList = getLocalStorageList(key);
+  valueList.push(value);
+  window.localStorage.setItem(key, JSON.stringify(valueList));
+  return JSON.parse(window.localStorage.getItem(key));
+}
+
 let localStorageGetBtnElement = document.getElementById('localStorageGetBtn');
 localStorageGetBtnElement.addEventListener('click', function () {
   let cartList = getLocalStorageList('cart');
@@ -16,6 +23,11 @@ localStorageGetBtnElement.addEventListener('click', function () {
 
 let localStorageAddBtnElement = document.getElementById('localStorageAddBtn');
 localStorageAddBtnElement.addEventListener('click', function () {
-  let cartList = getLocalStorageList('cart');
+  let productDummySchema = {
+    product: prompt('상품?'),
+    price: prompt('가격?'),
+  };
+
+  let cartList = addLocalStorageList('cart', productDummySchema);
   console.log(cartList);
 });
