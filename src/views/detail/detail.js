27b7db9e5c8dbc3e008ed356fa9d98d3bbbf6f41
cyberjam <jamsince1994@naver.com>
2022-05-27 22:54:11 +0900

    getLocalStorage 작성

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,21 +1,12 @@
-const dbReq = indexedDB.open('shopping', 1);
-let db;
-dbReq.addEventListener('success', function (event) {
-  db = event.target.result;
-});
-
-dbReq.addEventListener('error', function (event) {
-  const error = event.target.error;
-  console.log('error', error.name);
-});
-
-dbReq.addEventListener('upgradeneeded', function (event) {
-  db = event.target.result;
-  let oldVersion = event.oldVersion;
-  if (oldVersion < 1) {
-    db.createObjectStore('order', {
-      keyPath: 'id',
-      autoIncrement: true,
-    });
+function getLocalStorage() {
+  let cartList = window.localStorage.getItem('cart');
+  if (cartList === null) {
+    window.localStorage.setItem('cart', JSON.stringify([]));
+    return window.localStorage.getItem('cart');
+  } else {
+    return window.localStorage.getItem('cart');
   }
-});
+}
+
+let localStorageBtnElement = document.getElementById('localStorageBtn');
+localStorageBtnElement.addEventListener('click', function () {});
