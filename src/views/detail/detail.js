7456aea99ef0fb6088d2600a86a8b26bb4d095a8
cyberjam <jamsince1994@naver.com>
2022-06-01 09:01:14 +0900

    inputCart test code

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -3,6 +3,7 @@ import {
   getLocalStorageListById,
   addLocalStorageList,
   editQuantityLocalStorageListById,
+  inputCart,
 } from '/useful-functions.js';
 
 let localStorageGetBtnElement = document.getElementById('localStorageGetBtn');
@@ -18,22 +19,5 @@ localStorageAddBtnElement.addEventListener('click', function () {
     product: prompt('상품?'),
     price: prompt('가격?'),
   };
-
-  const valueList = getLocalStorageList('cart');
-  const storeId = productDummySchema.id;
-  const StorageList = valueList.filter((e) => e.id == storeId);
-
-  if (StorageList === null) {
-    addLocalStorageList('cart', productDummySchema);
-  } else {
-    let quantityValue = parseInt(
-      getLocalStorageListById('cart', storeId).quantity
-    );
-    quantityValue++;
-    if (quantityValue >= 99) {
-      editQuantityLocalStorageListById('cart', storeId, 99);
-    } else {
-      editQuantityLocalStorageListById('cart', storeId, quantityValue);
-    }
-  }
+  inputCart(productDummySchema.id, productDummySchema);
 });
