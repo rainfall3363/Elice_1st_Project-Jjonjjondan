78747bc51958bf7911caac00e3d0ea4a6ab07ac6
Circlewee <pnj1118@naver.com>
2022-06-04 10:34:41 +0900

    최종 버그 수정 및 검색 기능 구현

--- src/views/products/products.js
+++ src/views/products/products.js
@@ -7,6 +7,11 @@ import {
   changetoAdmin,
 } from '/useful-functions.js';
 
+//query String 값 가져오기
+const params = new URLSearchParams(document.location.search);
+const paramsCategoryName = params.get('categoryName');
+const paramsProductName = params.get('searchWord');
+
 init();
 
 async function init() {
@@ -17,6 +22,7 @@ async function init() {
   productsData();
   const data = await categoryData();
   categorymenuRender(data);
+  addEvent();
 }
 
 async function categoryData() {
@@ -43,23 +49,31 @@ function categorymenuRender(data) {
           `
       );
     }
-    if (paramscategoryName === categoryName) {
+    if (paramsCategoryName === categoryName) {
       const changecategoryname = document.getElementById('changecategoryname');
       changecategoryname.innerText = `${categoryName}`;
     }
   });
 }
 
-//query String 값 가져오기
-const params = new URLSearchParams(document.location.search);
-const paramscategoryName = params.get('categoryName');
-
 //데이터 받아오기
 async function productsData() {
   const productSection = document.getElementById('productSection');
-  const allContents = document.getElementById('allContents');
+  let data;
+
   try {
-    const data = await Api.get('/api/product/list');
+    if (paramsProductName) {
+      const searchInput = document.querySelector('#searchInput');
+      searchInput.value = paramsProductName;
+
+      data = await Api.get(
+        '/api/product/search',
+        `?searchWord=${paramsProductName}`
+      );
+    } else {
+      data = await Api.get('/api/product/list');
+    }
+
     await data.forEach((elem) => {
       const category = elem.categoryName;
       const title = elem.title;
@@ -67,42 +81,42 @@ async function productsData() {
       const maker = elem.maker;
       const price = addCommas(Number(elem.price));
       const productId = elem._id;
-      if (category === paramscategoryName) {
+      if (category === paramsCategoryName) {
         productSection.insertAdjacentHTML(
           'beforeend',
           `
-    <div class="box">
-      <a href="/productDetail?productId=${productId}">
-        <div class="product">
+          <div class="box">
+          <a href="/productDetail?productId=${productId}">
+          <div class="product">
           <img src="${imageURL}" id="productImage" alt="">
-        </div>
-        <div class="product">
-         <p class="description">${maker}</p>
+          </div>
+          <div class="product">
+          <p class="description">${maker}</p>
         </div>
         <div class="product">
           <p class="title">${title}</p>
         </div>
         <div class="product">
-          <p class="price is-font-orange">${price}원</p>
+        <p class="price is-font-orange">${price}원</p>
         </div>
-      </a>
+        </a>
     </div>
-   
+    
     `
         );
       }
-      if (paramscategoryName === null) {
+      if (paramsCategoryName === null) {
         productSection.insertAdjacentHTML(
           'beforeend',
           `
   <div class="box">
     <a href="/productDetail?productId=${productId}">
-      <div class="product">
-        <img src="${imageURL}" id="productImage" alt="">
+    <div class="product">
+    <img src="${imageURL}" id="productImage" alt="">
       </div>
       <div class="product">
        <p class="description">${maker}</p>
-      </div>
+       </div>
       <div class="product">
         <p class="title">${title}</p>
       </div>
@@ -117,5 +131,26 @@ async function productsData() {
   } catch (err) {
     console.error(err.stack);
     alert(`상품 정보를 불러오는 데 실패하였습니다.${err.message}`);
+  }
+}
+
+function addEvent() {
+  const searchButton = document.querySelector('.search-button');
+  const searchInput = document.querySelector('#searchInput');
+
+  searchButton.addEventListener('click', searchResult);
+  searchInput.addEventListener('keyup', searchResult);
+}
+
+function searchResult(event) {
+  const searchInput = document.querySelector('#searchInput');
+  const productName = searchInput.value;
+
+  if (event.key === 'Enter') {
+    if (productName.length < 2) {
+      alert('검색어를 2글자 이상 입력해주세요.');
+    } else {
+      window.location.href = `/products/?searchWord=${productName}`;
+    }
   }
 }
